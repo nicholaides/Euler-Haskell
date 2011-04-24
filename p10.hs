6e50fd8ad79284955eca9d6ace39d04ab32d6573
Mike Nicholaides <mike.nicholaides@gmail.com>
2011-04-24 13:56:36 -0400

    Fixing the bug in my solution p10.
    Again, it was the Int vs Integer thing

--- p10.hs
+++ p10.hs
@@ -1,16 +1,16 @@
 import Data.Array
 
-type PrimeList = Array Int Bool
+type PrimeList = Array Integer Bool
 
 tail' :: [a] -> [a]
 tail' [] = []
 tail' l  = tail l
 
-multiples :: Int -> Int -> [Int]
+multiples :: Integer -> Integer -> [Integer]
 multiples x max = tail' $ takeWhile (<= max) [x, (x+x) ..]
 
-bigNum = 2000000
--- bigNum =    10
+-- bigNum = 2000000
+bigNum =    2000000
 
 rangeTop = fromIntegral $ round $ sqrt $ fromIntegral bigNum
 
@@ -18,7 +18,7 @@ theBounds = (2, bigNum)
 theRange  = [2..bigNum]
 allTrue   = zip theRange (repeat True)
 
-addToArr :: PrimeList -> Int -> PrimeList
+addToArr :: PrimeList -> Integer -> PrimeList
 addToArr arr x =
   if arr ! x then -- if x is marked as prime in the array
     -- then mark all multiples as non-prime
@@ -31,10 +31,10 @@ foldl' f z (x:xs) = let z' = z `f` x
                     in seq z' $ foldl' f z' xs
 
 primesList :: PrimeList -> PrimeList
--- primesList arr = foldl' addToArr arr [2..rangeTop]
-primesList arr = foldl' addToArr arr theRange
+primesList arr = foldl' addToArr arr [2..rangeTop]
+-- primesList arr = foldl' addToArr arr theRange
 
-primes :: PrimeList -> [Int]
+primes :: PrimeList -> [Integer]
 primes arr = map (\ (x, _) -> x) $ filter (\ (x, bool) -> bool) $ assocs arr
 
 main = do
